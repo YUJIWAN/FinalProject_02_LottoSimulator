315eeaf1dedf796b7c1843e6f1bf2608c109800d
YUJIWAN <61855068+YUJIWAN@users.noreply.github.com>
2020-03-24 10:36:16 +0900

    당첨번호 정렬 + TextView 출력

--- app/src/main/java/kr/co/tjoeun/finalproject_02_lottosimulator/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/finalproject_02_lottosimulator/MainActivity.java
@@ -6,11 +6,17 @@ import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import kr.co.tjoeun.finalproject_02_lottosimulator.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
 
+    List<TextView> winNumTxtList = new ArrayList<>();
     int[] winLottoNumArr = new int[6];
     int bounsNum = 0;
     ActivityMainBinding binding = null;
@@ -19,7 +25,7 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         setupEvents();
         setValues();
@@ -42,11 +48,17 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setValues() {
 
+        winNumTxtList.add(binding.winLottoNumTxt01);
+        winNumTxtList.add(binding.winLottoNumTxt02);
+        winNumTxtList.add(binding.winLottoNumTxt03);
+        winNumTxtList.add(binding.winLottoNumTxt04);
+        winNumTxtList.add(binding.winLottoNumTxt05);
+        winNumTxtList.add(binding.winLottoNumTxt06);
+
     }
 
 
-
-    void makeWinLottoNum(){
+    void makeWinLottoNum() {
 
 //        6개의 숫자(배열) + 보너스번호 1개 int변수
 //         => 이 함수에서만이 아니라, 다른곳에서도 쓸 예정
@@ -55,46 +67,54 @@ public class MainActivity extends BaseActivity {
 //        당첨번호 + 보너스번호를 모두0 으로 초기화.
 //        (이미 뽑은 번호가 있다면 모두 날려라)
 
-        for ( int i = 0; i < winLottoNumArr.length; i++){
+        for (int i = 0; i < winLottoNumArr.length; i++) {
             winLottoNumArr[i] = 0;
         }
         bounsNum = 0;
 //        로또 번호 6개 생성
 //         1~45여야함 + 중복 허용 X
 
-        for(int i = 0; i < winLottoNumArr.length; i++){
+        for (int i = 0; i < winLottoNumArr.length; i++) {
 //            1~45의 숫자를 랜덤으로 뽑고
 //            중복이 아니라면 => 당첨번호로 선정
 //            중복이라면 다시 뽑자 => 중복이 아닐때까지 계속 뽑자.
 
-            while(true){
+            while (true) {
 
 //                0+1 <= (Math.random()*45)+1 < 1*45+1
 
 //                1~45의 정수를 램덤으로 뽑아서 임시 저장.
 //                이 숫자가 중복검사를 통과하면 사용, 아니면 다시.
-                int randomNum = (int)(Math.random()*45+1);
+                int randomNum = (int) (Math.random() * 45 + 1);
 
 //                중보검사? 당첨번호 전부와 randomNum을 비교.
 //                하나라도 같은 탈락.
 
                 boolean isDupOk = true; //중복검사 변수
-                for(int winNum : winLottoNumArr){
-                    if(winNum == randomNum){
+                for (int winNum : winLottoNumArr) {
+                    if (winNum == randomNum) {
                         isDupOk = false; //탈락!
                         break;
                     }
                 }
-                if(isDupOk){
+                if (isDupOk) {
                     winLottoNumArr[i] = randomNum;
-                    Log.i("당첨번호",randomNum+"");
+                    Log.i("당첨번호", randomNum + "");
                     break; // 무한반복 탈출
                 }
-
             }
+        }
 
+//        6개의 당첨번호를 작은 숫자부터 정렬.
+        Arrays.sort(winLottoNumArr);
+
+        for (int i = 0 ; i < winLottoNumArr.length; i++){
+
+            winNumTxtList.get(i).setText(winLottoNumArr[i]+"");
 
         }
+
+
 
     }
 }
