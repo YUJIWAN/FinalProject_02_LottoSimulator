168a20c8da745822fb28aa2437aa982d9cb27297
YUJIWAN <61855068+YUJIWAN@users.noreply.github.com>
2020-03-24 10:01:22 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/finalproject_02_lottosimulator/MainActivity.java
@@ -0,0 +1,29 @@
+package kr.co.tjoeun.finalproject_02_lottosimulator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import kr.co.tjoeun.finalproject_02_lottosimulator.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
